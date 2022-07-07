f4190c0c3750ed2fcb30e408ef98cf0a7cbe372c
shahaf <shahaf0205@gmail.com>
2022-07-07 13:17:05 +0300

    finished Q2

--- Intro/Source.c
+++ Intro/Source.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	printEmpire(53);
+	printf("%d\n", GetFibonacciVal(3));
 	return 0;
+}
+int GetFibonacciVal(int index) {
+	//index starts from 0
+	int i = 0;
+	int num1 = 0;
+	int num2 = 1;
+	int temp;
+	while (i < index) {
+		temp = num1;
+		num1 += num2;
+		num2 = temp;
+		i++;
+	}
+	return num1;
 }
 int printEmpire(int num) {
 	int i;
